2dd90db6e37a0396d551d42c73b7fd1ad14f2176
kev-69 <bismarkobuobi19@gmail.com>
2025-01-30 11:48:24 +0000

    Login page completed with styles

--- street-and-culture/src/pages/Login.js
+++ street-and-culture/src/pages/Login.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Mail, Lock } from 'lucide-react'
 
 import './Login.css'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login attempted with", formData);
+  }
+
+
   return (
-    <div>
-      
+    <div className='login-container'>
+      <div className="login-box">
+        <h1>Login</h1>
+        <p className="login-subtitle">Welcome back! Please login to your account</p>
+
+        <form onSubmit={handleSubmit} className='login-form'>
+          <div className="input-group">
+            <Mail className='input-icon' />
+            <input 
+              type="email"
+              id='email'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              required
+              placeholder='' 
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+
+          <div className="input-group">
+            <Lock className='input-icon' />
+            <input 
+              type="password" 
+              id='password'
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              required
+              placeholder=''
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+
+          <div className="form-options">
+            <label className="remember-me">
+              <input type="checkbox" />Remember me
+            </label>
+            <a href="#" className='forgot-password'>Forgot Password</a>
+          </div>
+          <button type='submit' className='login-btn'>Login</button>
+        </form>
+        <p className="register-link">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
     </div>
   )
 }
