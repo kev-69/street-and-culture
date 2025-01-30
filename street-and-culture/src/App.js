7b259167e499c365d8c5004d306a4f51f648640d
kev-69 <bismarkobuobi19@gmail.com>
2025-01-30 11:36:21 +0000

    added login and register to entry file

--- street-and-culture/src/App.js
+++ street-and-culture/src/App.js
@@ -7,6 +7,8 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import Blogs from "./pages/Blogs";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 // Brands pages import
 import Urban from "./pages/brands/Urban";
@@ -21,15 +23,17 @@ function App() {
       <div className="App">
           <Layout>
             <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/97Urban" element={<Urban />} />
-            <Route path="/60Fellas" element={<Fellas />} />
-            <Route path="/FreeTheYouth" element={<FTY />} />
-            <Route path="/Mindset" element={<Mindset />} />
-            <Route path="/KevWCP" element={<KevWCP />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/97Urban" element={<Urban />} />
+              <Route path="/60Fellas" element={<Fellas />} />
+              <Route path="/FreeTheYouth" element={<FTY />} />
+              <Route path="/Mindset" element={<Mindset />} />
+              <Route path="/KevWCP" element={<KevWCP />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
             </Routes>
           </Layout>
       </div>
