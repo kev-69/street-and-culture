3994425b3f07bc6e387282261adbba37436d8383
kev-69 <bismarkobuobi19@gmail.com>
2025-02-05 21:22:56 +0000

    Sample data

---
+++ street-and-culture/src/helpers/data.js
@@ -0,0 +1,147 @@
+import fty1Front from '../assets/brands-assets/FTY/FTY05_front.png';
+import fty1Back from '../assets/brands-assets/FTY/FTY05_back.png';
+import ubran1Front from '../assets/brands-assets/97Urban/P02_front.png';
+import ubran1Back from '../assets/brands-assets/97Urban/P02_back.png';
+import Kev1WCPOne from '../assets/brands-assets/Santa WCP/3da32b119fe4f1c7ae4709721cf1478f.jpg'
+import Kev1WCPTwo from '../assets/brands-assets/Santa WCP/7ad2090e48ad311a6bef64232439dfbe.jpg'
+import ubran2Front from '../assets/brands-assets/97Urban/P01_front.png';
+import ubran2Back from '../assets/brands-assets/97Urban/P01_back.png';
+import Kev2WCPOne from '../assets/brands-assets/Santa WCP/8e34a5ffe0df8b0072e0d2f09de0324b.jpg'
+import Kev2WCPTwo from '../assets/brands-assets/Santa WCP/aa2c2eb4eb55970ae7c3dcb5dee69057.jpg'
+import ubran3Front from '../assets/brands-assets/97Urban/zxz_front.png';
+import ubran3Back from '../assets/brands-assets/97Urban/zxz_back.png';
+
+
+
+const bestSellingData = [
+    {
+        id: 1,
+        brandName: "Free The Youth",
+        title: "FTY Basketball Jersey",
+        price: 89.99,
+        images: {
+            one: fty1Front,
+            two: fty1Back
+        },
+        category: "Clothing",
+        rating: 4.5,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 2,
+        brandName: "97 Urban",
+        title: "Tribe Blue NFL Jersey",
+        price: 79.99,
+        images: {
+            one: ubran1Front,
+            two: ubran1Back,
+        },
+        category: "Clothing",
+        rating: 4.3,
+        salesCount: 160,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 3,
+        brandName: "Kevlar Cool Pants",
+        title: "Raw Retro Edge",
+        price: 49.99,
+        images: {
+          one: Kev1WCPOne,
+          two: Kev1WCPTwo
+        },
+        category: "Clothing",
+        rating: 4.7,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 4,
+        brandName: "97 Urban",
+        title: "Tribe Pink NFL Jersey",
+        price: 59.99,
+        images: {
+          one: ubran2Front,
+          two: ubran2Back
+        },
+        category: "Clothing",
+        rating: 4.5,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 5,
+        brandName: "Kevlar Cool Pants",
+        title: "Faded Blue Black Butterflies",
+        price: 49.99,
+        images: {
+          one: Kev2WCPOne,
+          two: Kev2WCPTwo
+        },
+        category: "Clothing",
+        rating: 4.7,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 6,
+        brandName: "97 Urban",
+        title: "Z X Z",
+        price: 59.99,
+        images: {
+          one: ubran3Front,
+          two: ubran3Back
+        },
+        category: "Clothing",
+        rating: 4.4,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 7,
+        brandName: "Mindset",
+        title: "Mindset 1957",
+        price: 29.99,
+        images: {
+          one: ubran3Front,
+          two: ubran3Back
+        },
+        category: "Clothing",
+        rating: 4.4,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+    {
+        id: 8,
+        brandName: "60 Fellas",
+        title: "Mindset 1957",
+        price: 29.99,
+        images: {
+          one: ubran3Front,
+          two: ubran3Back
+        },
+        category: "Clothing",
+        rating: 4.4,
+        salesCount: 150,
+        isInStock: true,
+        discount: 0,
+        tags: ["bestseller", "trending"]
+    },
+];
+
+export { bestSellingData };
